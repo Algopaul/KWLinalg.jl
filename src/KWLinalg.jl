d61a6ca23c00d06396d9edf26f7273cdb17809eb
Algopaul <72927083+Algopaul@users.noreply.github.com>
2022-07-01 20:53:50 +0200

    add getrf! with ipiv argument

--- src/KWLinalg.jl
+++ src/KWLinalg.jl
@@ -1,5 +1,44 @@
 module KWLinalg
 
-# Write your package code here.
+using LinearAlgebra
+import LinearAlgebra:
+    require_one_based_indexing,
+    chkstride1,
+    BlasInt,
+    LAPACK.chkargsok,
+    LAPACK.getrf!,
+    LinearAlgebra.BLAS.libblastrampoline
+
+for (getrf, dtype) in [(:dgetrf_, Float64), (:zgetrf_, ComplexF64)]
+    @eval begin
+        function getrf!(A::AbstractMatrix{$dtype}, ipiv::Vector{BlasInt})
+            require_one_based_indexing(A)
+            chkstride1(A)
+            m, n = size(A)
+            @assert length(ipiv) >= min(m, n)
+            lda = max(1, stride(A, 2))
+            info = Ref{BlasInt}()
+            ccall(
+                (LinearAlgebra.BLAS.@blasfunc($getrf), libblastrampoline),
+                Cvoid,
+                (
+                    Ref{BlasInt},
+                    Ref{BlasInt},
+                    Ptr{ComplexF64},
+                    Ref{BlasInt},
+                    Ptr{BlasInt},
+                    Ptr{BlasInt},
+                ),
+                m,
+                n,
+                A,
+                lda,
+                ipiv,
+                info,
+            )
+            chkargsok(info[])
+        end
+    end
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,24 @@
+using LinearAlgebra
 using KWLinalg
 using Test
 
 @testset "KWLinalg.jl" begin
-    # Write your tests here.
+    for n in [1, 10]
+        m = max(n - 2, 1) # Choose rectangular RHS
+        A = rand(n, n)
+        AC = deepcopy(A)
+        A2 = rand(n, n)
+        B = rand(n, m)
+        X = rand(n, m)
+        lu_D = lu!(A2)
+        A2 .= AC
+        LAPACK.getrf!(lu_D.factors, lu_D.ipiv)
+        ldiv_alloc = @allocated ldiv!(X, lu_D, B)
+        @test ldiv_alloc == 0
+        @test norm(X - A \ B) < 1e-16
+        A2 .= AC
+        b_alloc = @allocated LAPACK.getrf!(lu_D.factors, lu_D.ipiv)
+        # Upon second call, no memory is allocated.
+        @test b_alloc == 0
+    end
 end
